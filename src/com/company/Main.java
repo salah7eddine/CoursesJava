c55f50006934c7b8329c169553ae9cb95f3dd2c9
salah7eddine <abousaid7salah@gmail.com>
2021-03-20 10:58:09 +0100

    Create the Product Management Application

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.company.data.Product;
+
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product p1 = new Product();
+
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+
+
+        System.out.println(p1.getId() +" "+p1.getName()+" "+p1.getPrice());
+    }
+}
