84995b10df16ce86aa92185ec047bf95d5260bf1
salah7eddine <abousaid7salah@gmail.com>
2021-04-19 05:21:59 +0000

    Simulate Concurrent callers.. Part 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,69 @@ import com.company.data.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.company.data.Rating.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        ProductManger pm = ProductManger.getInstance(); 
 
-        ProductManger pm = ProductManger.getInstance(); //new ProductManger();
-        pm.printProductReport(101, "en-GB");
-        pm.printProductReport(102, "ru-RU");
+        AtomicInteger clinetCount = new AtomicInteger();
+
+        Callable<String> client = () -> {
+
+            String clientId = "Client " + clinetCount.incrementAndGet();
+            String threadName = Thread.currentThread().getName();
+            int productId = ThreadLocalRandom.current().nextInt(63) + 101;
+            String languageTag = ProductManger
+                    .getSupportedLocales()
+                    .stream()
+                    .skip(ThreadLocalRandom.current()
+                            .nextInt(4))
+                    .findFirst()
+                    .get();
+            StringBuilder log = new StringBuilder();
+            log.append(clientId +""+threadName+"\n-\tstart of log\t-\n");
+            log.append(pm.getDiscounts(languageTag)
+                    .entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() +"\t"+entry.getValue())
+                    .collect(Collectors.joining("\n")));
+
+            Product product = pm.reviewProduct(productId, FOUR_STAR, "Yet another review");
+            log.append(product != null ? "\nProduct " + productId+" reviewed\n" : "\nProduct " + productId+" not reviewed\n");
+            pm.printProductReport(productId, languageTag, clientId);
+          log.append("\n-\tend of log\t-\n");
+
+            return log.toString();
+        };
+
+        List<Callable<String>> clients = Stream.generate(() -> client).limit(5).collect(Collectors.toList());
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        try {
+            List<Future<String>> results = executorService.invokeAll(clients);
+            executorService.shutdown();
+            results.stream().forEach(result -> {
+                try {
+                    System.out.println(result.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Error retrieving client log", e);
+                }
+            });
+        } catch (InterruptedException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Error invoking clients", e);
+        }
+
+        //new ProductManger();
+       /* pm.printProductReport(101, "en-GB");*/
+       /* pm.printProductReport(102, "ru-RU");*/
 
        /* pm.createProduct(164, "Kombucha", BigDecimal.valueOf(1.99), NOT_RATED);
         pm.reviewProduct(164, TWO_STAR, "Looks like tea but is it ?");
@@ -75,7 +127,6 @@ public class Main {
         pm.printProducts(p -> p.getPrice().floatValue() < 2, (p1, p11) -> p11.getRating().ordinal() - p1.getRating().ordinal());
 
         pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating +"\t"+ discount));*/
-
 
 
     }
